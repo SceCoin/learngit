4c614cdb0b3f3904362b5bd114ea130716828fb0
SilverLeo <zuoxuanzhang@gmail.com>
2017-12-12 18:04:33 +0800

    add py1.py

---
+++ py1.py
@@ -0,0 +1,15 @@
+# file_path = 'D:\JustUseIt.txt'
+file_path = 'everyday.txt'
+with open(file_path) as file_object:
+    lines = file_object.readlines()
+
+string = ''
+for line in lines:
+    string += line.rstrip()
+
+birthday = input("enter your birthday, in the form mmddyy:")
+if birthday in string:
+    print("you birthday appears in the first million digits of pi!")
+    print(string.find(birthday))
+else:
+    print("your birthday does not appear in the first million digits of pi.")
